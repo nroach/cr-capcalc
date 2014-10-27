4d4658c60a98ac81f8e5b890322e082efbadb115
Nathan Roach <nathan@capitalready.com>
2014-10-27 17:38:45 -0500

    connect to Google API with fixed key and display data

--- script.js
+++ script.js
@@ -3,27 +3,29 @@ var app = angular.module('myApp',[]);
 app.controller("mainController", function($scope, $http){
     $scope.results = [];
     $scope.filterText = null;
-    $scope.availableCategories = [];
-    $scope.categoryFilter = null;
+    $scope.availableDiscounts = [];
+    $scope.discountFilter = null;
+
+    var apiKey = '1tjo0fhDHKd1CTDaiZ-V8sOsBGkb5AK62dlZxaWO7zRE';
 
     $scope.init = function() {
 
     //$http.jsonp('http://spreadsheets.google.com/feeds/list/0An3Rz0qTgCysdFRtd182eGc2dFJGTjRCOWlQYjVvNlE/od6/public/values?alt=json-in-script' + '&callback=JSON_CALLBACK').success(function(data) {
-    $http.jsonp('http://spreadsheets.google.com/feeds/list/1tjo0fhDHKd1CTDaiZ-V8sOsBGkb5AK62dlZxaWO7zRE/od6/public/values?alt=json-in-script' + '&callback=JSON_CALLBACK').success(function(data) {
-    //0An3Rz0qTgCysdFRtd182eGc2dFJGTjRCOWlQYjVvNlE
+    //$http.jsonp('http://spreadsheets.google.com/feeds/list/' + '1tjo0fhDHKd1CTDaiZ-V8sOsBGkb5AK62dlZxaWO7zRE' + '/od6/public/values?alt=json-in-script' + '&callback=JSON_CALLBACK').success(function(data) {
+    $http.jsonp('http://spreadsheets.google.com/feeds/list/' + apiKey + '/od6/public/values?alt=json-in-script' + '&callback=JSON_CALLBACK').success(function(data) {
 
         angular.forEach(data, function(value, index){
                 angular.forEach(value.entry, function(classes, index){
                     $scope.results.push(classes);
-                    angular.forEach(classes.gsx$categorie, function(category, index){
+                    angular.forEach(classes.gsx$discount, function(discount, index){
                         var exists = false;
-                        angular.forEach($scope.availableCategories, function(avCat, index){
-                            if (avCat == category) {
+                        angular.forEach($scope.availableDiscounts, function(avDsc, index){
+                            if (avDsc == discount) {
                                 exists = true;
                             }
                         });
                         if (exists === false) {
-                            $scope.availableCategories.push(category);
+                            $scope.availableDiscounts.push(discount);
 
                         }
                     });
@@ -38,8 +40,8 @@ app.controller("mainController", function($scope, $http){
 
     };
 
-    $scope.setCategoryFilter = function(category) {
-    $scope.categoryFilter = category;
+    $scope.setDiscountFilter = function(discount) {
+    $scope.discountFilter = discount;
 
     }
 
